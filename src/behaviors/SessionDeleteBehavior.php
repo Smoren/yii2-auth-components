bc9105a697a611b31b86210863514854dcfa4b3c
Smoren <smoren@PC103619.PKRVMN.local>
2021-06-09 15:10:41 +0300

    session delete behavior

---
+++ src/behaviors/SessionDeleteBehavior.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Smoren\Yii2\Auth\behaviors;
+
+use Smoren\Yii2\Auth\helpers\SessionHelper;
+use yii\base\Behavior;
+use yii\base\Event;
+use yii\db\ActiveRecord;
+
+/**
+ * Поведение для удаления файлов, истёкших сессий
+ */
+class SessionDeleteBehavior extends Behavior
+{
+    /**
+     * @inheritdoc
+     */
+    public function events()
+    {
+        return [ActiveRecord::EVENT_AFTER_DELETE => 'afterDelete'];
+    }
+
+    /**
+     * Действия события при удаление AR модели.
+     * Удаляет файл, истёкшей сессии
+     * @param Event $event
+     */
+    public function afterDelete(Event $event)
+    {
+        $sender = $event->sender;
+        SessionHelper::deleteSessionFile($sender->token);
+    }
+}
--- src/helpers/SessionHelper.php
+++ src/helpers/SessionHelper.php
@@ -2,6 +2,7 @@
 
 namespace Smoren\Yii2\Auth\helpers;
 
+use Smoren\Yii2\Auth\components\Session;
 use Yii;
 
 /**
@@ -17,5 +18,14 @@ class SessionHelper
     public static function getCurrentSession()
     {
         return Yii::$app->session;
+    }
+
+    public static function deleteSessionFile($token)
+    {
+        $filePath = Session::getFilePath($token);
+        if(file_exists($filePath) && is_file($filePath)) {
+            error_log("removed $filePath");
+            @unlink($filePath);
+        }
     }
 }
