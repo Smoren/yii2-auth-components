f908efeb521b58fbafd437dc8fcecaf52c1a4318
Smoren <ofigate@gmail.com>
2021-06-23 18:03:20 +0300

    remove session from db method

--- src/components/SessionManager.php
+++ src/components/SessionManager.php
@@ -4,6 +4,7 @@ namespace Smoren\Yii2\Auth\components;
 
 use Smoren\Yii2\Auth\exceptions\SessionException;
 use Yii;
+use yii\base\Exception;
 
 /**
  * Хэлпер для работы с сессиями пользователей
@@ -14,18 +15,31 @@ class SessionManager
      * @var Session
      */
     protected static $session;
+    /**
+     * @var string
+     */
     protected static $token;
 
+    /**
+     * @param Session $session
+     */
     public static function register(Session $session)
     {
         static::$session = $session;
     }
 
+    /**
+     * @param $token
+     */
     public static function setToken($token)
     {
         static::$token = $token;
     }
 
+    /**
+     * @throws SessionException
+     * @throws Exception
+     */
     public static function open()
     {
         if(!(static::$session instanceof Session)) {
@@ -35,16 +49,35 @@ class SessionManager
         static::$session->start(static::$token);
     }
 
+    /**
+     * Close session
+     */
     public static function close()
     {
         Yii::$app->session->close();
     }
 
+    /**
+     * Remove db session row
+     */
+    public static function delete()
+    {
+        static::close();
+        static::getCurrentSession()->getDbSession()->delete();
+    }
+
+    /**
+     * @return Session
+     */
     public static function getCurrentSession(): Session
     {
         return Yii::$app->session;
     }
 
+    /**
+     * @param null $token
+     * @throws Exception
+     */
     public static function deleteSessionFile($token = null)
     {
         $filePath = Session::getFilePath($token ?? static::$token);
